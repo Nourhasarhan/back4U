0a08effc661074460502c809236db5093bc96122
Nourhan Mahmoud Sarhan <92642003+Nourhasarhan@users.noreply.github.com>
2024-10-01 12:19:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, APIRouter
-import uvicorn
+from typing import Union
+
+from fastapi import FastAPI
 
 app = FastAPI()
-class HelloWorld():
-    def read_hello(self):
-        return {"data": "Hello World"}
-router = APIRouter()
-router.add_api_route('/api/v2/hello-world', 
-endpoint = HelloWorld().read_hello, methods=["GET"])
-app.include_router(router)
 
-if __name__ == "__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
